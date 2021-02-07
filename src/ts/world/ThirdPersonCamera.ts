e3642181e91a34a6ca1883d288ebba6496ad5e39
espisepi <sepinaco@gmail.com>
2021-02-07 20:17:42 +0100

    ThirdPersonCamera still not working well

--- src/ts/world/ThirdPersonCamera.ts
+++ src/ts/world/ThirdPersonCamera.ts
@@ -2,14 +2,14 @@ import * as THREE from 'three';
 
 export class ThirdPersonCamera {
 
-    public _target: any;
+    public _cameraOperator: any;
     public _camera: any;
     public _currentPosition: THREE.Vector3;
     public _currentLookat: THREE.Vector3;
 
-    constructor(target,camera) {
-      this._target = target;
-      this._camera = camera;
+    constructor(cameraOperator) {
+      this._cameraOperator = cameraOperator;
+      this._camera = cameraOperator.camera;
   
       this._currentPosition = new THREE.Vector3();
       this._currentLookat = new THREE.Vector3();
@@ -19,18 +19,18 @@ export class ThirdPersonCamera {
     _CalculateIdealOffset(zoomType = 0) {
       const idealOffset = new THREE.Vector3(0, 0.5, -2);
       const quaternion = new THREE.Quaternion();
-      quaternion.setFromEuler(this._target.rotation);
+      // quaternion.setFromEuler(this._cameraOperator.rotation);
       idealOffset.applyQuaternion(quaternion);
-      idealOffset.add(this._target.position);
+      idealOffset.add(this._cameraOperator.target);
       return idealOffset;
     }
   
     _CalculateIdealLookat() {
       const idealLookat = new THREE.Vector3(0, 0, 0);
       const quaternion = new THREE.Quaternion();
-      quaternion.setFromEuler(this._target.rotation);
+      // quaternion.setFromEuler(this._cameraOperator.rotation);
       idealLookat.applyQuaternion(quaternion);
-      idealLookat.add(this._target.position);
+      idealLookat.add(this._cameraOperator.target);
       return idealLookat;
     }
   
